a5a42c3032bba1347ad34d0119a22a551a25b27b
1sa_ahmed <adahmedt@gmail.com>
2022-07-27 11:19:57 -0400

    todo lomba problem

---
+++ function.js
@@ -0,0 +1,96 @@
+/**
+ * random id generate
+ */
+const randomId = () => Math.floor(Math.random() * 10000) + '_' + Date.now();
+
+/**
+ * sent alert
+ */
+const alertSimple = (sms, color = 'danger') => {
+  return `<p class="alert alert-${color} d-flex justify-content-between">${sms}<button data-bs-dismiss="alert" class="btn-close btn-sm"></button></p>`;
+};
+
+/**
+ * time runner
+ */
+const timerRunner = (endTime) => {
+  // getting real time and doing negative
+  let run = Math.floor(Math.abs(endTime - Date.now()));
+
+  // get time accordingly by run var
+  let total_sec = Math.floor(run / 1000),
+    total_min = Math.floor(total_sec / 60),
+    total_hr = Math.floor(total_min / 60),
+    total_day = Math.floor(total_hr / 24);
+
+  // get specific time from total time
+  let hr = total_hr - total_day * 24,
+    min = total_min - total_day * 24 * 60 - hr * 60,
+    sec = total_sec - total_day * 24 * 60 * 60 - hr * 60 * 60 - min * 60;
+
+  // lastly return time
+  if (endTime > Date.now()) {
+    return `${total_day} Day ${hr} Hour ${min} Min ${sec} Sec`;
+  } else {
+    return `[ <strong class="text-danger">Time over</strong> ]`;
+  }
+};
+
+/**
+ * create LS data
+ */
+const createLsdata = (key, data) => {
+  // store data to an empty array
+  let listing = [];
+
+  // checking if key exists if true make parse and store to localStorage.
+  localStorage.getItem(key) && (listing = JSON.parse(localStorage.getItem(key)));
+
+  // push now data to listing
+  listing.push(data);
+
+  // passing listing data to localStorage and by making json string
+  localStorage.setItem(key, JSON.stringify(listing));
+};
+
+/**
+ * creating apply ls data
+ */
+const applyLsData = (key) => {
+  if (localStorage.getItem(key)) {
+    return JSON.parse(localStorage.getItem(key));
+  } else {
+    return false;
+  }
+};
+
+/**
+ * update LS data
+ */
+const updataLsData = (key, list) => localStorage.setItem(key, JSON.stringify(list));
+
+/**
+ * progress bar
+ */
+const pBar = (startTime, endTime) => {
+  const run = endTime - startTime;
+  const now = endTime - Date.now();
+
+  // making progress bar after getting time
+  const pWidth = Math.floor((100 * now) / run);
+
+  let wid = '';
+
+  if (pWidth >= 0 && pWidth <= 33) {
+    wid = `width:${pWidth}%; background-color:red;`;
+  } else if (pWidth >= 34 && pWidth <= 66) {
+    wid = `width:${pWidth}%; background-color:orange;`;
+  } else if (pWidth >= 67 && pWidth <= 100) {
+    wid = `width:${pWidth}%; background-color:green;`;
+  } else {
+    wid = `width:100%; background-color:red;`;
+  }
+  console.log(pWidth);
+
+  return wid;
+};
---
+++ readme.js
@@ -0,0 +1,103 @@
+// get all items
+const todo_form = document.querySelector('#todo-form');
+const msg = document.querySelector('#msg');
+const listGroup = document.querySelector('ul.list-group');
+const cardFooter = document.querySelector('.card-footer');
+
+// submit form
+todo_form.onsubmit = (e) => {
+  e.preventDefault();
+
+  // get from data by destructuring way
+  const form_data = new FormData(e.target);
+  const { cl_name, pj_name, date, time } = Object.fromEntries(form_data.entries());
+
+  // set condition accordingly
+  if (!cl_name || !pj_name || !date || !time) {
+    msg.innerHTML = alertSimple('value is missing, please Check again!');
+  } else {
+    // make time
+    const startTime = Date.now(),
+      lastTime = new Date(date + ' ' + time),
+      endTime = lastTime.getTime();
+
+    // make rendom id
+    const randomId = Math.floor(Math.random() * 10000) + '_' + Date.now();
+
+    // sorting them
+    let singleData = { cl_name, pj_name, startTime, endTime, randomId };
+
+    // now pass data to ls
+    createLsdata('todo', singleData);
+    e.target.reset();
+    showMe();
+  }
+};
+
+// create function to show by html
+const showMe = () => {
+  // apply data from ls
+  const allData = applyLsData('todo');
+
+  // create an empty array
+  let data = '';
+
+  // no data condition
+  // (!allData || allData.length) == 0 ? (data = '') : '';
+  if (!allData || allData.length == 0) {
+    data = [];
+  }
+
+  // ls data show code
+  if (allData) {
+    allData.reverse().map((pro) => {
+      data += `
+      <li class="list-group-item shadow mt-3">
+        Client : ${pro.cl_name} | ${pro.pj_name} | Remain time :
+        <strong>${timerRunner(pro.endTime)}</strong>
+        <button deleteList="${pro.randomId}" class="close">&Cross;</button>
+        <span style="${pBar(pro.startTime, pro.endTime)}" class="progess-bar" ></span>
+      </li>`;
+    });
+  }
+
+  // console.log(pBar(84, 14));
+
+  // show data
+  listGroup.innerHTML = data;
+
+  // footer show data
+  cardFooter.innerHTML = `<p>Total number of project running is : <span>${
+    allData ? allData.length : 'NULL'
+  }</span></p>`;
+};
+showMe();
+
+setInterval(() => {
+  showMe();
+}, 1000);
+
+// delete feature
+listGroup.onclick = (e) => {
+  // const id = e;
+
+  if (e.target.hasAttribute('deleteList')) {
+    const id = e.target.getAttribute('deleteList');
+
+    // apply all data
+    const allData = applyLsData('todo');
+
+    //get id from data
+    const datay = allData.findIndex((data) => data.randomId == id);
+
+    // remove id from data
+    allData.splice(datay, 1);
+
+    // now update data again
+    updataLsData('todo', allData);
+
+    // show data
+    showMe();
+  }
+};
+showMe();
